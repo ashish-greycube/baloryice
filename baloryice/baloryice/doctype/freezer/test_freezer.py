ec6a3638d1cea276b3524d0afad2ad3bf226166e
ashish-greycube <mr.ashish.shah@gmail.com>
2021-04-12 15:09:47 +0200

    feat:freezer

---
+++ baloryice/baloryice/doctype/freezer/test_freezer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, greycube.in and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestFreezer(unittest.TestCase):
+	pass
